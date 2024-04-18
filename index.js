280fb1474597b87ee09ed3abc036c2720bc03090
Anna Baranek <annaxbaranek@gmail.com>
2024-04-18 10:31:30 +0200

    Xylofon js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Funguju')
+const audioFiles = {
+    'C': new Audio('tony/C.mp3'),
+    'D': new Audio('tony/D.mp3'),
+    'E': new Audio('tony/E.mp3'),
+    'F': new Audio('tony/F.mp3'),
+    'G': new Audio('tony/G.mp3'),
+    'A': new Audio('tony/A.mp3'),
+    'B': new Audio('tony/B.mp3')
+};
+
+const nadpis =  document.querySelector('h1')
+
+const playAudio = (fileName) => {
+    const audio = audioFiles[fileName];
+    audio.play();
+    nadpis.textContent = fileName;
+};
+
+
+document.querySelectorAll('.kamen').forEach(element => {
+    element.addEventListener('click', () => {
+        const fileName = element.textContent;
+        playAudio(fileName);
+    });
+});
+
